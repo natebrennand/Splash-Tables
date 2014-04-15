98f85aa27c7333f3b965957584b710a73b6d2f03
Nate Brennand <natebrennand@gmail.com>
2014-04-15 00:30:54 -0400

    refactor and implement get()

--- part1/Table.java
+++ part1/Table.java
@@ -1,24 +1,45 @@
 
-import java.lang.Math;
-import java.lang.Integer;
 
 /*
  *  Represents a "Splash" hashtable.
  */
 class Table {
-    private int bucketSize;         // B, number of elements per hash bucket
+    private Bucket[] buckets;       // B, number of elements per hash bucket
     private int numReinsertions;    // R, number of reinsertions before failure
-    private int numTableEntries;    // S, logarithmic size of table(N log 2)
-    private int[] hashMultipliers;  // h, number of random integers for the multiplicative hash fn
+    private Hash hashes;
 
-    public Table(int bucketSize, int numReinsertions, int numTableEntries, int numHashes){
-        this.bucketSize = bucketSize;
+    public Table(int bucketSize, int numReinsertions, int numTableEntries, int numHashes) {
         this.numReinsertions = numReinsertions;
-        this.numTableEntries = numTableEntries;
+        this.hashes = new Hash(numHashes);
 
-        this.hashMultipliers = new int[numHashes];
-        for (int i=0; i<numHashes; i++) {
-            this.hashMultipliers[i] = (int)(Math.random() * Integer.MAX_VALUE);
+        int numBuckets = 2^numTableEntries / bucketSize;
+        this.buckets = new Bucket[numBuckets];
+        for (int i=0; i<numBuckets; i++) {
+            this.buckets[i] = new Bucket(bucketSize);
         }
     }
+
+    public void insert(int key, int value) {
+        // try to insert
+        // dump then exit if exceeding recursion limit
+    }
+
+    /*  Get
+     *
+     *  @param: integer key
+     *  @return: value (>0) if exists, -1 otherwise
+     */
+    public int get(int key) {
+        int bucketIndex, value;
+        int[] hashes = this.hashes.Hash(key);
+        for (int i=0; i<hashes.length; i++) {
+            bucketIndex = (hashes[0] / hashes.length) % hashes.length;
+            value = this.buckets[bucketIndex].get(key);
+            if (value > 0) {
+                return value;
+            }
+        }
+        return -1;
+    }
 }
+
