22e482d83160e774a81a789edee8652aba419a98
Nate Brennand <natebrennand@gmail.com>
2014-04-27 13:08:15 -0400

    fixed hash

--- part1/Hash.java
+++ part1/Hash.java
@@ -12,6 +12,9 @@ class Hash {
         hashMultipliers = new int[numHashes];
         for (int i=0; i<numHashes; i++) {
             this.hashMultipliers[i] = (int)(Math.random() * Integer.MAX_VALUE);
+            if (this.hashMultipliers[i] < Integer.MAX_VALUE/2) {
+                i -= 1; // redo hash to ensure it is a high number
+            }
         }
         this.size = size;
     }
@@ -26,10 +29,21 @@ class Hash {
         */
         int[] hashes = new int[this.hashMultipliers.length];
         for(int i=0; i<hashes.length; i++) {
-            double hashIndex = ((this.hashMultipliers[i] * key) % Math.pow(2, 32))/Math.pow(2,24);
-            hashes[i] = Math.abs((int)(Math.floor(hashIndex)) % this.size);
+            hashes[i] = (int)(hashFn(key, this.hashMultipliers[i])) >>> (32-this.size);
         }
         return hashes;
     }
 
+    private double hashFn(int key, int multiplier) {
+        /*
+         * floor(m * frac(k*a))
+         * k: integer hash code
+         * a: real number
+         * frac: fn that returns the fractional part of a real number
+         *
+         */
+        double hashIndex = ((multiplier * key) % Math.pow(2, 32));
+        return Math.abs((int)(Math.floor(hashIndex)));
+    }
 }
+
