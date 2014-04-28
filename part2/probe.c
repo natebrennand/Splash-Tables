ff27f3a70870f9eebdaceaf26bb3fa33a66114bc
Nate Brennand <natebrennand@gmail.com>
2014-04-28 17:12:38 -0400

    start simplifying splash table loading

--- part2/probe.c
+++ part2/probe.c
@@ -11,4 +11,3 @@ int main (int argc, char *argv[])
 	}
 	splashtable(argv[1]);
 }
-
--- part2/splash.c
+++ part2/splash.c
@@ -1,40 +1,58 @@
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include "splash.h"
 #include "simd.h"
 
+
+const int BUCKET_SIZE = 4,
+	NUM_HASHES = 2;
+
+struct SplashTable
+{
+	int size;	// num elements = 2^size
+	int hashMultipliers[2];
+};
+
+
 int splashtable(char* file)
 {
 	FILE *dumpfile = fopen(file, "r");
 	if (dumpfile == NULL) {
-		printf( "Could not open file\n" );
+		printf("Could not open file\n");
 		return(-1);
-	} else {
-		int B, S, h, N;
-		fscanf(dumpfile, "%d %d %d %d", &B, &S, &h, &N);
-
-		int hashnum[h];
-		int size = pow(2, S-1);
-		int keys[size];
-		int payloads[size];
-		int i;
-		for(i=0;i<h;i++) {
-			fscanf(dumpfile, "%d", &(hashnum[i]));
-		}
-		for(i=0;i<size;i++) {
-			fscanf(dumpfile, "%d %d", &(keys[i]), &(payloads[i]));
-		}
-
-		printf("B: %d, S: %d, h: %d, N: %d\n", B, S, h, N);
-		for(i=0;i<h;i++) {
-			printf("hash: %d\n", hashnum[i]);
-		}
-		for(i=0;i<size;i++) {
-			printf("key: %d, payload: %d\n", keys[i], payloads[i]);
-		}
-
-		return 0;
-		// return probe(hashnum, keys, payloads);
 	}
+
+	int bucketSize, S, numHashes, N;
+	fscanf(dumpfile, "%d %d %d %d", &bucketSize, &S, &numHashes, &N);
+
+	// fail if bucket size and the number of hashes is not proper
+	if (bucketSize != BUCKET_SIZE || numHashes != NUM_HASHES) {
+		printf("Dump file must use a bucket size of %d and %d hashes\n.", BUCKET_SIZE, NUM_HASHES);
+		exit(1);
+	}
+
+	int hashnum[2];
+	int totalSize = pow(2, S-1);
+	int keys[totalSize];
+	int payloads[totalSize];
+
+	fscanf(dumpfile, "%d %d", &hashnum[0], &hashnum[1]);
+	for(int i=0;i<totalSize;i++) {
+		fscanf(dumpfile, "%d %d", &(keys[i]), &(payloads[i]));
+	}
+
+	printf("B: %d, S: %d, h: %d, N: %d\n", bucketSize, S, numHashes, N);
+	for(int i=0; i<numHashes; i++) {
+		printf("hash: %d\n", hashnum[i]);
+	}
+	for(int i=0; i<totalSize; i++) {
+		printf("key: %d, payload: %d\n", keys[i], payloads[i]);
+	}
+
+	return 0;
+	// return probe(hashnum, keys, payloads);
+
 	fclose(dumpfile);
 }
