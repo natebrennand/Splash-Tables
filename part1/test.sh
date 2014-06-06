301839b89104f5fb7fe997cbb72ddd0cc4925cf9
Nate Brennand <natebrennand@gmail.com>
2014-06-05 23:37:39 -0700

    testing scripts

---
+++ part1/test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+table_size=10
+
+bucket_size=$1
+hashes=$2
+reinsertions=$3
+
+inputfile="dump$table_size.txt"
+dumpfile=dumpfile
+filename="$bucket_size:$hashes:$reinsertions.test"
+
+echo "B:$bucket_size R:$reinsertions S:$table_size h:$hashes" > $filename
+for i in {1..1}
+do
+    python ../testing/random_key_value.py $table_size > $inputfile 
+    java Splash $bucket_size $reinsertions $table_size $hashes $inputfile $dumpfile < probe.txt >> $filename
+done
+
